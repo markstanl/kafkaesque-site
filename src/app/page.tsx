0002c24f75cf643726e1ca9a33171848dd716bac
markstanl <markgstanley1@gmail.com>
2024-12-08 17:07:24 -0600

    fixed small issue with width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,11 @@ const frijole = Frijole({
 export default function Home() {
     return (
         <div className={frijole.className}>
-            <div className={`w-screen h-screen bg-black text-white grid grid-cols-2`}>
+            <div className={`h-screen bg-black text-white`}>
                 <div className={'w-full h-full flex flex-col items-start justify-center p-6'}>
                     <h2 className={'text-4xl'}>HOW</h2>
                     <h1 className={'text-8xl'}>Kafkaesque</h1>
                     <h2 className={'text-4xl'}>are YOU feeling?</h2>
-                </div>
-                <div>
                 </div>
             </div>
         </div>
